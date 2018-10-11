3a76cc4ff2960c29f0dd78a7f35a9187c559e70f
Ilya Timofeev <il.tmfv@gmail.com>
2018-10-11 20:50:29 +0300

    Add styles, pictures, TextRow component

--- src/ekg/core.cljs
+++ src/ekg/core.cljs
@@ -5,11 +5,7 @@
 
 (println "This text is printed from src/ekg/core.cljs. Go ahead and edit it and see reloading in action.")
 
-(defonce app-state (r/atom {:a 0 :b 0 :res 0}))
-
-(defonce a (r/cursor app-state [:a]))
-(defonce b (r/cursor app-state [:b]))
-(defonce res (r/cursor app-state [:res]))
+(defonce app-state (r/atom {}))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
@@ -17,20 +13,19 @@
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
 
-(defn- calc []
-  (reset! res (+ (js/parseFloat @a) (js/parseFloat @b))))
+(defn TextRow [label value*]
+  [:tr
+   [:td label]
+   [:td
+    [:input {:type "text" :on-change #(reset! value* (-> % .-target .-value))}]]])
 
 (defn app []
-  [:div {:style {:display "flex" :flex-direction "column" :width 300}}
-   [:span (str "a + b = " @res)]
-   [:input {:type "number"
-            :value @a
-            :on-change (fn [e]
-                         (reset! a (-> e .-target .-value)))}]
-   [:input {:type "number"
-            :value @b
-            :on-change (fn [e]
-                         (reset! b (-> e .-target .-value)))}]
-   [:button {:on-click calc} "Расчитать"]])
+  [:div.page
+   [:img {:src "/images/header.png"}]
+   [:table
+    [:tbody
+     [TextRow "P (+)" (r/atom "")]
+     [TextRow "P (-)" (r/atom "")]]]
+   [:img {:src "/images/footer.png"}]])
 
 (r/render [app] (.getElementById js/document "app"))
